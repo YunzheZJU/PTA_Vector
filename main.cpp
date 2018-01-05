52f118198f9f8bd01e8ae20c610f4484f0dfa349
Yunzhe <iLoveLive@honoka.moe>
2018-01-05 23:30:35 +0800

    Done.

--- main.cpp
+++ main.cpp
@@ -9,62 +9,79 @@ private:
     T *m_elements;
     int m_size;
 public:
-    Vector() = default;
+    Vector() {
+        m_size = 0;
+        m_elements = new T;
+    }
 
-    Vector(int num);
+    Vector(int num) : m_size(num) {
+        m_elements = new T[num];
+        for (int i = 0; i < num; i++) {
+            m_elements[i] = i;
+        }
+    }
 
-    Vector(const Vector &);
+    Vector<T> &operator=(const Vector<T> &that) {
+        delete[] m_elements;
+        m_size = that.get_size();
+        m_elements = new T[m_size];
+        for (int i = 0; i < that.get_size(); i++) {
+            m_elements[i] = that.m_elements[i];
+        }
+        return *this;
+    }
 
-    Vector &operator=(const Vector &);
+    const T &operator[](int index) const {
+        if (index >= 0 && index < m_size) {
+            return m_elements[index];
+        } else {
+            return -1;
+        }
+    }
 
-    T &operator[](int index);
+    const int add(const T &element) {
+        Vector<T> old;
+        old = *this;
+        delete[] m_elements;
+        m_size++;
+        m_elements = new T[m_size];
+        for (int i = 0; i < m_size - 1; i++) {
+            m_elements[i] = old.m_elements[i];
+        }
+        m_elements[m_size - 1] = element;
+        delete[] old.m_elements;
+        return m_size - 1;
+    }
 
-    const int add(const T &element);
+    void remove(int index) {
+        Vector old;
+        old = *this;
+        delete[] m_elements;
+        m_size--;
+        m_elements = new T[m_size];
+        for (int i = 0; i < index; i++) {
+            m_elements[i] = old[i];
+        }
+        for (int i = index + 1; i < m_size; i++) {
+            m_elements[i - 1] = old[i];
+        }
+        delete[] old.m_elements;
+    }
 
-    void remove(int index);
-
-    const int get_size();
+    const int get_size() const {
+        return m_size;
+    }
 
     virtual ~Vector() = default;
 };
 
-template<class T>
-Vector<T>::Vector(int num) {
-
-}
-
-template<class T>
-Vector<T>::Vector(const Vector &) {
-
-}
-
-template<class T>
-Vector &Vector<T>::operator=(const Vector &) {
-    return <#initializer#>;
-}
-
-template<class T>
-T &Vector<T>::operator[](int index) {
-    return <#initializer#>;
-}
-
-template<class T>
-const int Vector<T>::add(const T &element) {
-    return 0;
-}
-
-template<class T>
-void Vector<T>::remove(int index) {
-
-}
-
-template<class T>
-const int Vector<T>::get_size() {
-    return 0;
-}
-
 int main() {
     Vector<int> vint;
+//    Vector<int> vint_2 = Vector<int>(20);
+//    vint_2.add(15);
+//    for (int i = 0; i < 21; i++) {
+//        cout << "vint_2[" << i << "]: " << vint_2[i] << endl;
+//    }
     int n, m;
     cin >> n >> m;
     for (int i = 0; i < n; i++) {
